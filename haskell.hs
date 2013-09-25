77e1589669c36570ecc98ea06a1b377731d54ad0
root <root@debian.(none)>
2013-09-25 17:43:42 +0300

    Haskell evenSum

--- haskell.hs
+++ haskell.hs
@@ -1,1 +1,13 @@
-main = print "Hello, world!"
+evenSum :: [Integer] -> Integer
+
+evenSum l = accumSum 0 l
+
+accumSum n l = if l == []
+                  then n
+                  else let x = head l 
+                           xs = tail l 
+                       in if even x
+                              then accumSum (n+x) xs
+                              else accumSum n xs
+
+main = print (evenSum [1..5])
