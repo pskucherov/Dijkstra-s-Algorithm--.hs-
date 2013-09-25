0e6c4517575247931c787c3eac51b997d5211a31
root <root@debian.(none)>
2013-09-25 19:54:13 +0300

    Done. Dijkstra in python (js -> py)

--- python.py
+++ python.py
@@ -1,11 +1,13 @@
-graph    = [];
-shortWay = [];
+graph    = []; 
+shortWay = []; 
 
-for x in range(6):
+n = 6;  
+
+for i in range(n):
     graph.append([]);
     shortWay.append(float('inf'));
-    for y in range(6):
-        graph[x].append(0);
+    for j in range(n):
+        graph[i].append(0);
 
 
 graph[0][1] = 7;
@@ -22,6 +24,22 @@ graph[3][4] = 6;
 
 graph[4][5] = 9;
 
+def findShortWay(start):
+  shortWay[start] = 0;
+  l = len(shortWay);
+  for i in range(l):
+    for j in range(l): 
+      if (i != j) and (graph[i][j] != 0): 
+        
+        if shortWay[i] != float('inf'):
+          way = graph[i][j] + shortWay[i]
+        else:
+          way = graph[i][j]
+        
+        if shortWay[j] > way and way > 0:
+          shortWay[j] = way;
 
-print graph, shortWay;
+findShortWay(0);
+print shortWay;
+
 
